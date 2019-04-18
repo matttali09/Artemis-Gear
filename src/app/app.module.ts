84db1f323a431641878569e28b6ef2613e6e7fed
matttali09 <matttaliancich09@gmail.com>
2019-04-17 23:52:33 -0400

    blew up old repo to create mdbootstrap that uses scss also added new nav bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { JumbotronComponent } from './jumbotron/jumbotron.component';
+import { HomeComponent } from './home/home.component';
+import { FeaturedItemsComponent } from './home/featured-items/featured-items.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    JumbotronComponent,
+    HomeComponent,
+    FeaturedItemsComponent
   ],
   imports: [
     BrowserModule,
+    MDBBootstrapModule.forRoot(),
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/jumbotron/jumbotron.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-jumbotron',
+  templateUrl: './jumbotron.component.html',
+  styleUrls: ['./jumbotron.component.scss']
+})
+export class JumbotronComponent implements OnInit {
+  title = 'Artemis Gear';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
