d0265d0f19d91862394be9189a10acd82a2c7c3d
matttali09 <matttaliancich09@gmail.com>
2019-04-22 14:22:57 -0400

    added forsales and precentages and out of stock divs for the featured items cards

--- src/app/home/featured-items/featured-items.component.ts
+++ src/app/home/featured-items/featured-items.component.ts
@@ -9,48 +9,75 @@ export class FeaturedItemsComponent implements OnInit {
   cards = [
     {
       title: 'Diamond Bow',
-      description: 'Some quick example text to build on the card title and make up the bulk of the card content',
-      img: 'assets/images/bow1.png'
+      description: 'Some quick example text to build on the card title and make up the bulk of the card content add a bunch of ffnnnn text here to test the height adjusments for cards you know what im saying brother like a lotta text to really test this shiz hard',
+      img: 'assets/images/bow1.png',
+      sale: true,
+      salePercentage: 25,
+      outOfStock: true,
     },
     {
       title: 'Card Title 2',
       description: 'Some quick example text to build on the card title and make up the bulk of the card content',
-      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg'
+      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg',
+      sale: true,
+      salePercentage: 20,
+      outOfStock: false,
     },
     {
       title: 'Card Title 3',
       description: 'Some quick example text to build on the card title and make up the bulk of the card content',
-      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg'
+      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg',
+      sale: true,
+      salePercentage: 15,
+      outOfStock: true,
     },
     {
       title: 'Card Title 4',
       description: 'Some quick example text to build on the card title and make up the bulk of the card content',
-      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg'
+      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg',
+      sale: true,
+      salePercentage: 15,
+      outOfStock: true,
     },
     {
       title: 'Diamond Bow',
       description: 'Some quick example text to build on the card title and make up the bulk of the card content',
-      img: 'assets/images/bow1.png'
+      img: 'assets/images/bow1.png',
+      sale: true,
+      salePercentage: 20,
+      outOfStock: false,
     },
     {
       title: 'Card Title 6',
       description: 'Some quick example text to build on the card title and make up the bulk of the card content',
-      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg'
+      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg',
+      sale: true,
+      salePercentage: 25,
+      outOfStock: true,
     },
     {
       title: 'Card Title 7',
       description: 'Some quick example text to build on the card title and make up the bulk of the card content',
-      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg'
+      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg',
+      sale: true,
+      salePercentage: 25,
+      outOfStock: false,
     },
     {
       title: 'Card Title 8',
       description: 'Some quick example text to build on the card title and make up the bulk of the card content',
-      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg'
+      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg',
+      sale: true,
+      salePercentage: 20,
+      outOfStock: false,
     },
     {
       title: 'Diamond Bow',
       description: 'Some quick example text to build on the card title and make up the bulk of the card content',
-      img: 'assets/images/bow1.png'
+      img: 'assets/images/bow1.png',
+      sale: true,
+      salePercentage: 15,
+      outOfStock: true,
     },
   ];
   slides: any = [[]];
